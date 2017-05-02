a0af248616fa5b80faabb5a02b440021ae25555c
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-05-02 18:24:01 +0000

    Some changes made in app.js and package.json

--- Parser_Project/app.js
+++ Parser_Project/app.js
@@ -1,21 +1,26 @@
 var express= require("express"),
-    bodyParser= require("body-parser"),
-    app= express();
+    bodyParser= require("body-parser"), 
+    app= express(),
+    useragent = require('express-useragent');
     
     
     app.set('view engine','ejs');
-    
+     
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(useragent.express());
     
-    
+  
     app.get('/',function(req,res){
-        res.send('This is home page ');
-    });
+        var headers= req.headers;
+        var useragent= req.useragent;
+       res.render('home',{headers: headers, useragent: useragent });
+      
+         console.log(req.useragent);
+      
+    }); 
     
     
-    
-    
-    
+    app.listen(3000);
     app.listen(process.env.PORT,process.env.IP,function(){
         console.log('Server is good and running.');
     });
